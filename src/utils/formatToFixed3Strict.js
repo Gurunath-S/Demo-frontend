804d91ecdd3e73d0845958d93b7446d753cf0e5d
Gurunath-S <guruthedev20@gmail.com>
2025-11-12 10:59:10 +0530

    1st thing

--- src/utils/formatToFixed3Strict.js
+++
@@ -1,5 +0,0 @@
-export const formatToFixed3Strict = (value) => {
-  if (value === "" || value === null || value === undefined) return "";
-  if (isNaN(value)) return "-";
-  return parseFloat(value).toFixed(3);
-};
